7b16e0f140e61f97fa474e244e4589efe6ed0c4c
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-09-15 11:43:35 +0600

    refactor: make JobCard component dynamic

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router";
+
 const JobCard = ({ job }) => {
-  const { category, job_title } = job;
+  const { _id, category, job_title, description, max_price, min_price,deadline } = job;
+  console.log(job)
   return (
-    <div className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
+    <Link to={`/job/${_id}`} className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
       <div className='flex items-center justify-between'>
         <span className='text-xs font-light text-gray-800 '>
-          Deadline: 20/12/2024
+          Deadline: {deadline}
         </span>
         <span className='px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full '>
           {category}
@@ -16,14 +19,14 @@ const JobCard = ({ job }) => {
           {job_title}
         </h1>
 
-        <p className='mt-2 text-sm text-gray-600 '>
-          Lorem ipsum dolor sit adipisicing elit...
+        <p title={description} className='mt-2 text-sm text-gray-600 '>
+         {description.substring(0,70)}...
         </p>
         <p className='mt-2 text-sm font-bold text-gray-600 '>
-          Range: $100 - $150
+          Range: ${min_price} - ${max_price}
         </p>
       </div>
-    </div>
+    </Link>
   )
 }
 
